f12f72116cc35692161e20a0c50a7e7aead1f300
cadoretg <gael.cadoret@ingin.fr>
2014-01-20 19:30:35 +0100

    Ajout de la fonctionnalité d'ajout d'une tâche

--- js/app.js
+++ js/app.js
@@ -2,7 +2,27 @@
 
 function TodoCtrl($scope) {
 
-    $scope.remaining = 30;
-    console.log($scope);
+    $scope.todos = [
+        {
+            name : 'Tâche incomplète',
+            completed : false
+        },
+        {
+            name : 'Tâche complète',
+            completed : true
+        }
+    ]
 
+    $scope.removeTodo = function(index) {
+        $scope.todos.splice(index,1);
+    }
+
+    $scope.addTodo = function() {
+        $scope.todos.push({
+            name : $scope.newtodo,
+            completed : false
+        });
+
+        $scope.newtodo = '';
+    }
 }
