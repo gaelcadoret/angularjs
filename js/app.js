00ae0568b4e9a26b8d28cf0206f9e38d367ffa3b
cadoretg <gael.cadoret@ingin.fr>
2014-01-21 14:30:45 +0100

    ajoute le background + fichier json

--- js/app.js
+++ js/app.js
@@ -1,23 +1,42 @@
+var app = angular.module('todo', []);
 
+app.directive('ngBlur', function() {
+    return function(scope, elem, attrs) {
+        elem.bind('blur', function() {
+            console.warn(attrs.ngBlur);
+            scope.$apply(attrs.ngBlur);
+        })
+    };
+});
 
-function TodoCtrl($scope) {
+app.controller('TodoCtrl',function ($scope, filterFilter, $http) {
 
-    $scope.todos = [
-        {
-            name : 'Tâche incomplète',
-            completed : false
-        },
-        {
-            name : 'Tâche complète',
-            completed : true
-        }
-    ]
+    $scope.todos = [];
+    $scope.placeholder = "Chargement...";
+
+    $http.get('data/json/todos.json').success(function(data) {
+        console.log('ajax request in success.');
+
+        $scope.todos = data;
+        $scope.placeholder = 'Ajouter une nouvelle tâche';
+    });
+
+    $scope.$watch('todos', function() {
+        console.log('$watch event triggered.');
+
+        $scope.remaining = filterFilter($scope.todos, {completed:false}).length;
+        $scope.allchecked = !$scope.remaining;
+    }, true);
 
     $scope.removeTodo = function(index) {
+        console.log('removeTodo called.');
+
         $scope.todos.splice(index,1);
     }
 
     $scope.addTodo = function() {
+        console.log('addTodo called.');
+
         $scope.todos.push({
             name : $scope.newtodo,
             completed : false
@@ -25,4 +44,16 @@ function TodoCtrl($scope) {
 
         $scope.newtodo = '';
     }
-}
+
+    $scope.editTodo = function(todo) {
+        console.log('editTodo called.');
+        todo.editing = false;
+    }
+
+    $scope.checkAllTodo = function(allchecked){
+        console.log('checkAllTodo called.');
+        $scope.todos.forEach(function(todo){
+            todo.completed = allchecked;
+        });
+    }
+});
